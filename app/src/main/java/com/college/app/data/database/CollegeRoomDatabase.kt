0d1ac770ead8d962c2d647076c3235eb04a0eed9
4shutosh <4shutoshsingh@gmail.com>
2021-12-23 22:20:27 +0530

    todo entity added

--- app/src/main/java/com/college/app/data/database/CollegeRoomDatabase.kt
+++ app/src/main/java/com/college/app/data/database/CollegeRoomDatabase.kt
@@ -1,6 +1,15 @@
 package com.college.app.data.database
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.college.app.data.entities.TodoItem
 
-// todo add type convertor here
+@Database(
+    entities = [
+        TodoItem::class
+    ],
+    version = 3,
+    exportSchema = false
+)
 abstract class CollegeRoomDatabase : RoomDatabase(), CollegeAppDatabase
+// todo add type convertor here
