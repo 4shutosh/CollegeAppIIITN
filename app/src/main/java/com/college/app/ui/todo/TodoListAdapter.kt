e8f96a8ff0f81c66d29f57e315251a7548f878cb
4shutosh <4shutoshsingh@gmail.com>
2022-01-18 21:43:46 +0530

    view types introduced

--- app/src/main/java/com/college/app/ui/todo/TodoListAdapter.kt
+++ app/src/main/java/com/college/app/ui/todo/TodoListAdapter.kt
@@ -5,46 +5,104 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.college.app.databinding.ListItemTodoBinding
+import com.college.app.databinding.ListItemTodoCompleteBinding
+import com.college.app.databinding.ListItemTodoIncompleteBinding
 import com.college.app.utils.extensions.executeAfter
+import com.college.app.utils.extensions.strike
 
 class TodoListAdapter constructor(private val clickListener: TodoItemClickListener) :
-    ListAdapter<TodoListViewState, TodoListAdapter.ViewHolder>(TodoListDiffCallback) {
+    ListAdapter<TodoListViewState, RecyclerView.ViewHolder>(TodoListDiffCallback) {
 
     private var lastExpandedItemPosition = -1 // no position
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
-        ListItemTodoBinding.inflate(
-            LayoutInflater.from(parent.context), parent, false
-        )
-    )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            TODO_LIST_VIEW_TYPE_COMPLETE -> ViewHolderItemComplete(
+                ListItemTodoCompleteBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+            )
+            TODO_LIST_VIEW_TYPE_INCOMPLETE -> ViewHolderItemIncomplete(
+                ListItemTodoIncompleteBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+            )
+            else -> throw Exception("TodoListAdapter: Wrong View Type found!")
+        }
+    }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val currentItem = getItem(position)
-        holder.binding.executeAfter {
-            viewState = currentItem
-            itemPosition = position
-            actionHandler = clickListener
-        }
+        when (getItemViewType(position)) {
+            TODO_LIST_VIEW_TYPE_COMPLETE -> {
+                val viewHolder = holder as ViewHolderItemComplete
+                viewHolder.binding.executeAfter {
+                    viewState = currentItem
+                    itemPosition = position
+                    actionHandler = clickListener
+                }
 
-        holder.binding.listItemTodoBg.setOnClickListener {
-            if (lastExpandedItemPosition != -1 && lastExpandedItemPosition != position
-                && lastExpandedItemPosition < currentList.size
-            ) {
-                val lastItem = getItem(lastExpandedItemPosition)
-                lastItem.isExpanded = false
-                notifyItemChanged(lastExpandedItemPosition)
+                viewHolder.binding.listItemTodoBg.setOnClickListener {
+                    if (lastExpandedItemPosition != -1 && lastExpandedItemPosition != position
+                        && lastExpandedItemPosition < currentList.size
+                    ) {
+                        val lastItem = getItem(lastExpandedItemPosition)
+                        lastItem.isExpanded = false
+                        notifyItemChanged(lastExpandedItemPosition)
+                    }
+                    lastExpandedItemPosition = if (currentItem.isExpanded) -1
+                    else viewHolder.absoluteAdapterPosition
+
+                    currentItem.isExpanded = (!currentItem.isExpanded)
+                    clickListener.onTodoItemClick(currentItem, position)
+                }
+
+                viewHolder.binding.listItemTodoTitle.strike = true
             }
-            lastExpandedItemPosition = if (currentItem.isExpanded) -1
-            else holder.absoluteAdapterPosition
+            TODO_LIST_VIEW_TYPE_INCOMPLETE -> {
+                val viewHolder = holder as ViewHolderItemIncomplete
 
-            currentItem.isExpanded = (!currentItem.isExpanded)
-            clickListener.onTodoItemClick(currentItem, position)
-        }
+                viewHolder.binding.executeAfter {
+                    viewState = currentItem
+                    itemPosition = position
+                    actionHandler = clickListener
+                }
 
-        holder.binding.listItemTodoNotifySwitch.setOnCheckedChangeListener { _, notify ->
-            clickListener.onTodoItemNotifyClicked(currentItem, notify)
+                viewHolder.binding.listItemTodoBg.setOnClickListener {
+                    if (lastExpandedItemPosition != -1 && lastExpandedItemPosition != position
+                        && lastExpandedItemPosition < currentList.size
+                    ) {
+                        val lastItem = getItem(lastExpandedItemPosition)
+                        lastItem.isExpanded = false
+                        notifyItemChanged(lastExpandedItemPosition)
+                    }
+                    lastExpandedItemPosition = if (currentItem.isExpanded) -1
+                    else viewHolder.absoluteAdapterPosition
+
+                    currentItem.isExpanded = (!currentItem.isExpanded)
+                    clickListener.onTodoItemClick(currentItem, position)
+                }
+
+                viewHolder.binding.listItemTodoNotifySwitch.setOnCheckedChangeListener { _, notify ->
+                    clickListener.onTodoItemNotifyClicked(currentItem, notify)
+                }
+            }
         }
+    }
+
+    override fun getItemViewType(position: Int) =
+        if (getItem(position).isCompleted) TODO_LIST_VIEW_TYPE_COMPLETE
+        else TODO_LIST_VIEW_TYPE_INCOMPLETE
+
+    inner class ViewHolderItemComplete(val binding: ListItemTodoCompleteBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    inner class ViewHolderItemIncomplete(val binding: ListItemTodoIncompleteBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    companion object {
+        const val TODO_LIST_VIEW_TYPE_INCOMPLETE = 0
+        const val TODO_LIST_VIEW_TYPE_COMPLETE = 1
     }
 
     interface TodoItemClickListener {
@@ -57,10 +115,9 @@ class TodoListAdapter constructor(private val clickListener: TodoItemClickListen
         fun onTodoItemEditTime(viewState: TodoListViewState, position: Int)
 
         fun onTodoItemNotifyClicked(viewState: TodoListViewState, notify: Boolean)
-    }
 
-    inner class ViewHolder(val binding: ListItemTodoBinding) :
-        RecyclerView.ViewHolder(binding.root)
+        fun onTodoItemMarkAsDone(viewState: TodoListViewState, position: Int)
+    }
 }
 
 object TodoListDiffCallback : DiffUtil.ItemCallback<TodoListViewState>() {
@@ -74,7 +131,7 @@ object TodoListDiffCallback : DiffUtil.ItemCallback<TodoListViewState>() {
 
 }
 
-enum class TodoListTypes(val title: String) {
+enum class TodoListFilterTypes(val title: String) {
     ALL("All"),
     TODAY("Today"),
     WEEK("Week"),
